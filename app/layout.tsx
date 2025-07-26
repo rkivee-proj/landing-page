7130a419732aac5aa63f7cd8e0b056cdbf9c8364
harshit960 <raj.harshit960@gmail.com>
2025-07-26 12:26:42 +0530

    tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 import "./globals.css";
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >      
       <GoogleAnalytics gaId="G-ELBPDQCX2Q" />
+      <GoogleTagManager gtmId="G-ELBPDQCX2Q" />
 
           {children}
       </body>
